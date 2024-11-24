05432e26603c81199638311d14d326f68a300898
Nooberieno <nooberieno.dev@gmail.com>
2024-11-24 19:13:44 +0100

    its looking like i want, now to make basic collapsing functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,10 @@
 </script>
 
 <main>
-  <div class="test"> je moeder</div>
+  <div class="sidebar">
+    <div class="sidebar-button">yooo</div>
+    <div class="sidebar-item-content">git content i guess</div>
+  </div>
   <div id="editor">
     <AlgiraEditor />
   </div>
@@ -12,9 +15,13 @@
 
 <style>
   main{
+    font-family: "Hack Nerd Font" monospace;
+    position: absolute;
     display: flex;
     padding: none;
     max-width: 100%;
     height: 100%;
+    width: 100%;
+    flex-direction: row;
   }
 </style>
