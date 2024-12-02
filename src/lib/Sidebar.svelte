89deec0ab9eab5d471d32f5d6543dd55a2c8ab1d
Nooberieno <nooberieno.dev@gmail.com>
2024-12-02 16:02:28 +0100

    sidebar, with working icons that can be clicked and open the sidebar

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -7,8 +7,13 @@
 </script>
 
 <div class="sidebar">
-    <div class="sidebar-button">yooo
-      <button onclick={toggle_sidebar}>toggle</button>
-    </div>
-    <div class="sidebar-item-content">sidebar content</div>
+  <div class="sidebar-buttons">
+    <button class="sidebar-button" onclick={toggle_sidebar}>&#xea83;</button>
+    <button class="sidebar-button" onclick={toggle_sidebar}>&#xea68;</button>
+    <button class="sidebar-button" onclick={toggle_sidebar}>&#xeae6;</button>
+    <button class="sidebar-button" onclick={toggle_sidebar}>&#xebdc;</button>
+    <button class="sidebar-button" onclick={toggle_sidebar}>&#xf2c0</button>
+    <button class="sidebar-button" onclick={toggle_sidebar}>&#xeb51</button>
   </div>
+  <div class="sidebar-item-content">sidebar content</div>
+</div>
