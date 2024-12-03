2b18a4c6f36c58c364531e05bf949d141365457c
Nooberieno <nooberieno.dev@gmail.com>
2024-12-03 16:19:46 +0100

    style  changes for sidebar content items and making start to change editor language mode when a button is pressed

--- src/lib/sidebar/Lang-Change.svelte
+++ src/lib/sidebar/Lang-Change.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import { change_language } from "../../utils/language";
+    import "../../style/sidebar/content.css"
 </script>
 
-
-<button>Python</button>
+<div id="content-container">
+    <button class="content-button" onclick={() => change_language("python")}>Python</button>
+</div>
