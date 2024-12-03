bd525d402516790e0f895a48ac5fd44731f18c0e
Nooberieno <nooberieno.dev@gmail.com>
2024-12-03 12:32:22 +0100

    changed style of the user icon and moved the user and settings buttons down

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -12,7 +12,7 @@
     <button class="sidebar-button" onclick={toggle_sidebar}>&#xea68;</button>
     <button class="sidebar-button" onclick={toggle_sidebar}>&#xeae6;</button>
     <button class="sidebar-button" onclick={toggle_sidebar}>&#xebdc;</button>
-    <button class="sidebar-button" onclick={toggle_sidebar}>&#xf2c0</button>
+    <button style="margin-top: 40rem;" class="sidebar-button" onclick={toggle_sidebar}>&#xf2be</button>
     <button class="sidebar-button" onclick={toggle_sidebar}>&#xeb51</button>
   </div>
   <div class="sidebar-item-content">sidebar content</div>
