327444a694105f6bc432906544ef96c83944b1c4
Nooberieno <nooberieno.dev@gmail.com>
2024-12-10 11:39:55 +0100

    switching to the current_file_path store and cleaning imports in the Editor Component

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -1,23 +1,24 @@
 <script lang='ts'>
-    import '../style/cm_styles.css'
+    //Style sheets
+    import '../style/cm_styles.css';
+    //Library imports
     import type { Unsubscriber } from 'svelte/store';
     import { onMount } from "svelte";
     import { EditorState, Compartment, type Extension } from "@codemirror/state";
     import { EditorView } from "codemirror";
     import { basicSetup } from "codemirror";
+    import { oneDark } from '@codemirror/theme-one-dark';
+    //Utility functionality
     import { read_file, write_file, current_file_path } from "../utils/filesystem";
     import { open_file_dialog, save_file_dialog } from '../utils/dialog';
     import { open_dialog_bindings, save_existing_file } from "../utils/keybinds";
-    import { change_language, current_lang, get_language_by_extension } from "../utils/language" 
-    import { oneDark } from '@codemirror/theme-one-dark'
+    import { change_language, current_lang, get_language_by_extension } from "../utils/language"; 
 
-    let view:EditorView;
-    let current_filepath: string
-    const language_compartment: Compartment =  new Compartment;
-    const theme_compartment: Compartment = new Compartment;
-    let unsubscribe_lang: Unsubscriber
-    let unsubscribe_file: Unsubscriber
-
+    let view: EditorView;
+    const language_compartment: Compartment = new Compartment();
+    const theme_compartment: Compartment = new Compartment();
+    let unsubscribe_lang: Unsubscriber;
+    let unsubscribe_file: Unsubscriber;
 
     onMount(() => {
         view = new EditorView({
@@ -34,69 +35,72 @@
 
         unsubscribe_lang = current_lang.subscribe(async (lang) => {
             if (lang) {
-                let lang_func: Extension | null = await change_language(lang)
-                if (lang_func){
+                let lang_func: Extension | null = await change_language(lang);
+                if (lang_func) {
                     view.dispatch({
                         effects: language_compartment.reconfigure(lang_func)
-                    })
+                    });
                 }
             }
-        })
+        });
 
         unsubscribe_file = current_file_path.subscribe(async (path) => {
-            console.log("path updated to: ", path)
+            console.log("path updated to: ", path);
             if (path) {
                 const text = await read_file(path as string);
-                if (text){
-                    console.log("Content loaded succesfully", text)
+                current_lang.set(get_language_by_extension(path))
+                if (text) {
+                    console.log("Content loaded successfully", text);
                     view.dispatch({
-                    changes: { from: 0, to: view.state.doc.length, insert: text}
-                })
+                        changes: { from: 0, to: view.state.doc.length, insert: text }
+                    });
                 }
             }
-        })
+        });
 
         const open_file = async () => {
             const file_path = await open_file_dialog();
             if (file_path) {
                 const text = await read_file(file_path as string);
-                if (text){
-                    console.log("Content loaded succesfully", text)
-                    current_filepath = file_path
+                if (text) {
+                    console.log("Content loaded successfully", text);
+                    current_file_path.set(file_path);
                     view.dispatch({
-                    changes: { from: 0, to: view.state.doc.length, insert: text}
-                })
-            }
-                if (current_filepath){
-                    let lang_func:string|null = get_language_by_extension(current_filepath)
-                    if (lang_func){
-                        current_lang.set(lang_func)
+                        changes: { from: 0, to: view.state.doc.length, insert: text }
+                    });
+                }
+                if (file_path) {
+                    let lang_func: string | null = get_language_by_extension(file_path);
+                    if (lang_func) {
+                        current_lang.set(lang_func);
                     }
                 }
             }
-        }
-        open_dialog_bindings(view, open_file)
+        };
+        open_dialog_bindings(view, open_file);
 
         const save_file = async () => {
-            if (current_filepath) {
-                write_file(current_filepath as string, view.state.doc.toString())
-            }else {
-                let file_path = await save_file_dialog()
-                if (file_path){
-                    write_file(file_path as string, view.state.doc.toString())
-                    current_lang.set(get_language_by_extension(file_path))
-                    current_filepath = file_path
+            let file_path: string | null = null
+            const unsubscribe: Unsubscriber = current_file_path.subscribe(path => {file_path = path});
+            unsubscribe();
+            if (file_path) {
+                write_file(file_path as string, view.state.doc.toString());
+            } else {
+                file_path = await save_file_dialog();
+                if (file_path) {
+                    write_file(file_path as string, view.state.doc.toString());
+                    current_file_path.set(file_path);
                 }
             }
-        }
-        save_existing_file(view, save_file)
+        };
+        save_existing_file(view, save_file);
 
         return () => {
-            if (view){
-                view.destroy()
+            if (view) {
+                view.destroy();
             }
-            unsubscribe_lang()
-            unsubscribe_file()
+            unsubscribe_lang();
+            unsubscribe_file();
         };
-    })
+    });
 </script>
