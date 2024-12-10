20cb03fc635f6f83f7cd54f586929942a0dd74cb
Nooberieno <nooberieno.dev@gmail.com>
2024-12-10 21:55:30 +0100

    Addition of all currently planned sidebar components, although they are only placeholders. Addition of a terminal component, which for now also does nothing, but it is at least looking acceptable

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
   // Style sheet
   import '../style/sidebar/general.css';
+  import '../style/terminal.css'
   
   import type { Component } from "svelte";
   //Component imports that will dedicate the content of the sidebar based on what button is active
   import LangChange from "./sidebar/Lang-Change.svelte";
   import Directory from "./sidebar/Directory.svelte";
+  import RunAndDebug from './sidebar/Run&Debug.svelte';
+  import Extension from './sidebar/Extension.svelte';
+  import Git from './sidebar/Git.svelte';
 
     let SidebarContent = $state<Component>()
 
     function toggle_sidebar(){
         const sidebar = document.querySelector(".sidebar")
         sidebar?.classList.toggle("expanded")
+        
     }
 
 </script>
@@ -20,13 +25,13 @@
   <div class="sidebar-buttons">
     <div class="sidebar-buttons-top">
       <button class="sidebar-button" onclick={() => {toggle_sidebar(); SidebarContent = Directory}}>&#xea83;</button>
-      <button class="sidebar-button" onclick={toggle_sidebar}>&#xea68;</button>
-      <button class="sidebar-button" onclick={toggle_sidebar}>&#xeae6;</button>
-      <button class="sidebar-button" onclick={toggle_sidebar}>&#xebdc;</button>
+      <button class="sidebar-button" onclick={() => {toggle_sidebar(); SidebarContent = Git}}>&#xea68;</button>
+      <button class="sidebar-button" onclick={() => {toggle_sidebar(); SidebarContent = Extension}}>&#xeae6;</button>
+      <button class="sidebar-button" onclick={() => {toggle_sidebar(); SidebarContent = RunAndDebug}}>&#xebdc;</button>
       <button class="sidebar-button" onclick={() => {toggle_sidebar(); SidebarContent = LangChange}}>&#xf100d</button>
     </div>
     <div class="sidebar-buttons-top">
-      <button class="sidebar-button down" onclick={toggle_sidebar}>&#xea85</button>
+      <button class="sidebar-button down" onclick={() => {document.querySelector(".terminal")?.classList.toggle("expanded")}}>&#xea85</button>
       <button class="sidebar-button down" onclick={toggle_sidebar}>&#xf2be</button>
       <button class="sidebar-button down" onclick={toggle_sidebar}>&#xeb51</button>
     </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script>
   import AlgiraEditor from '$lib/Editor.svelte'
   import Sidebar from '$lib/Sidebar.svelte'
+  import Terminal from '$lib/Terminal.svelte';
   import '../style/style.css'
 </script>
 
 <main>
   <Sidebar />
-  <div id="editor">
-    <AlgiraEditor />
+  <div class="container">
+    <div id="editor">
+      <AlgiraEditor />
+    </div>
+    <Terminal />
   </div>
 </main>
