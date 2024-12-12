bf6a328fbb870736e66cfb27bee179bb238be982
Nooberieno <nooberieno.dev@gmail.com>
2024-12-12 14:38:31 +0100

    prototype of a tab system, creating new tabs currently isnt possible

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -16,14 +16,17 @@
     import { open_dialog_bindings, save_existing_file } from "../utils/keybinds";
     import { change_language, current_lang, get_language_by_extension } from "../utils/language"; 
 
-    let view: EditorView;
+    let editors: EditorView[] = $state([])
+    let current_tab: number = $state(0)
+    let tabs: string[] = $state(['test', "untitled"])
     const language_compartment: Compartment = new Compartment();
     const theme_compartment: Compartment = new Compartment();
     let unsubscribe_lang: Unsubscriber;
     let unsubscribe_file: Unsubscriber;
 
     onMount(() => {
-        view = new EditorView({
+        tabs.forEach((tab, index) => {
+            const editor = new EditorView({
             state: EditorState.create({
                 doc: '',
                 extensions: [
@@ -32,14 +35,40 @@
                     theme_compartment.of(oneDark),
                 ],
             }),
-            parent: document.getElementById('editor')!
+            parent: document.getElementById(`editor-${index}`)!
         });
+        editors.push(editor)
+        }) 
+
+        editors.forEach((editor, index) => {
+            if (index !== current_tab){
+                editor.dom.style.display = 'none'
+            }
+        })
+
+        const switch_tab = (index: number) => {
+            editors.forEach((editor, i) => {
+                if(i === index){
+                    
+                }else{
+                    editor.dom.style.display = 'none'
+                }
+            })
+            current_tab = index
+        }
+
+        tabs.forEach((tab, index) => {
+            const tab_element = document.getElementById(`tab-${index}`)
+            if(tab_element){
+                tab_element.addEventListener('click', () => switch_tab(index))
+            }
+        })
 
         unsubscribe_lang = current_lang.subscribe(async (lang) => {
             if (lang) {
                 let lang_func: Extension | null = await change_language(lang);
                 if (lang_func) {
-                    view.dispatch({
+                    editors[current_tab].dispatch({
                         effects: language_compartment.reconfigure(lang_func)
                     });
                 }
@@ -52,8 +81,8 @@
                 const text = await read_file(path as string);
                 current_lang.set(get_language_by_extension(path))
                 if (text) {
-                    view.dispatch({
-                        changes: { from: 0, to: view.state.doc.length, insert: text }
+                    editors[current_tab].dispatch({
+                        changes: { from: 0, to: editors[current_tab].state.doc.length, insert: text }
                     });
                 }
             }
@@ -72,11 +101,11 @@
             const unsubscribe: Unsubscriber = current_file_path.subscribe(path => {file_path = path});
             unsubscribe();
             if (file_path) {
-                write_file(file_path as string, view.state.doc.toString());
+                write_file(file_path as string, editors[current_tab].state.doc.toString());
             } else {
                 file_path = await save_file_dialog();
                 if (file_path) {
-                    write_file(file_path as string, view.state.doc.toString());
+                    write_file(file_path as string, editors[current_tab].state.doc.toString());
                     current_file_path.set(file_path);
                 }
             }
@@ -84,11 +113,26 @@
         save_existing_file(save_file);
 
         return () => {
-            if (view) {
-                view.destroy();
+            if (editors) {
+                editors.forEach(editor => {
+                    editor.destroy()
+                })
             }
             unsubscribe_lang();
             unsubscribe_file();
         };
     });
 </script>
+
+<style>
+  </style>
+
+<div>
+    {#each tabs as tab, index}
+      <div class={`tab ${current_tab === index ? 'active' : ''}`} id={`tab-${index}`}>{tab}</div>
+    {/each}
+  </div>
+  
+  {#each tabs as _, index}
+    <div class={`editor ${current_tab === index ? 'active' : ''}`} id={`editor-${index}`}></div>
+  {/each} 
