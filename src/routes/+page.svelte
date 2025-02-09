3b3fbbd43e90a024415b8a78f64087614bce42b3
Nooberieno <nooberieno.dev@gmail.com>
2025-02-09 11:32:18 +0100

    Testing Active Extensions
    Simple reference implementation to see if adding extensions to active_extensions work and it does

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,11 @@
   import TabBar from "../components/TabBar.svelte";
   import TabContent from "../components/TabContent.svelte";
   import { createTab } from "../lib/tabs.svelte";
+  import { tabs } from "../lib/tabs.svelte";
+  import { active_extensions } from "$lib/cm-extensions.svelte";
+  import { python } from "@codemirror/lang-python";
   createTab(Editor)
+  active_extensions[tabs[0].id] = [python()]
 </script>
 
 <main class="container">
