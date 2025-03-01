5c88a81c1d2d41df13e84eedbca88d6b467db6c3
Nooberieno <nooberieno.dev@gmail.com>
2025-03-01 22:11:02 +0100

    Terminal Keystrokes
    Terminal now registers keystrokes and handles backspace and enter properly. Sending a command does nothing yet as it isnt hooked up to the rust backend yet.

--- src/components/Terminal.svelte
+++ src/components/Terminal.svelte
@@ -5,13 +5,36 @@
     import { onMount } from "svelte";
     import { Terminal } from "@xterm/xterm";
 
+    let term_container: HTMLDivElement
+    let output: string = $state("")
 
     onMount(() => {
         const term = new Terminal();
     
-        term.open(document.getElementById("terminal"))
-        term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ")
+        term.open(term_container)
+        term.write("$: ")
+
+        term.onData((data) => {
+            const code = data.charCodeAt(0)
+
+            if(code === 13){
+                term.write("\r\n")
+                console.log("Command entered: ", output)
+                output = ""
+                term.write("$: ")
+                return;
+            }
+            if(code === 127){
+                if(output.length > 0){
+                    output = output.slice(0, -1)
+                    term.write("\b \b")
+                }
+                return
+            }
+            output += data
+            term.write(data)
+        })
     })
 </script>
 
-<div id="terminal"></div>
+<div bind:this={term_container} id="terminal"></div>
