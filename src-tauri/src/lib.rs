c1020d4e2383a1e6709855d5266b2ff1617c628e
Nooberieno <nooberieno.dev@gmail.com>
2025-03-02 15:34:10 +0100

    Proper Terminal Implementation
    A proper terminal implementation inspired by marc2332/tauri-terminal
    
    The terminal uses portable-pty on the backend and xtermjs on the frontend to give a proper integrated terminal experience
    
    Even though the terminal is function, it is in its early stages and further work is needed to properly integrate it with the surrounding infrastructure

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,31 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::{io::BufReader, sync::Arc};
+use portable_pty::{native_pty_system, PtySize};
+use tauri::async_runtime::Mutex as AsyncMutex;
+
+mod terminal;
+use terminal::TermState;
+
 pub fn run() {
+    let pty_sys = native_pty_system();
+
+    let pty_pair = pty_sys.openpty(PtySize { rows: 24, cols: 80, pixel_width: 0, pixel_height: 0 }).unwrap();
+
+    let reader = pty_pair.master.try_clone_reader().unwrap();
+    let writer = pty_pair.master.take_writer().unwrap();
+
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
+        .manage(TermState {
+            pty_pair: Arc::new(AsyncMutex::new(pty_pair)),
+            writer: Arc::new(AsyncMutex::new(writer)),
+            reader: Arc::new(AsyncMutex::new(BufReader::new(reader))),
+        })
+        .invoke_handler(tauri::generate_handler![terminal::create_shell_process, terminal::pty_write, terminal::pty_read, terminal::pty_resize])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Terminal.svelte
+++ src/components/Terminal.svelte
@@ -4,36 +4,54 @@
 
     import { onMount } from "svelte";
     import { Terminal } from "@xterm/xterm";
+    import { FitAddon } from "@xterm/addon-fit"
+    import { invoke } from "@tauri-apps/api/core";
 
     let term_container: HTMLDivElement
-    let output: string = $state("")
 
     onMount(() => {
-        const term = new Terminal();
-    
+        const fit = new FitAddon()
+        const term = new Terminal({
+            cursorBlink: true,
+            fontFamily: "Hack Mono",
+            fontSize: 8
+        });
+        term.loadAddon(fit)
         term.open(term_container)
-        term.write("$: ")
-
-        term.onData((data) => {
-            const code = data.charCodeAt(0)
-
-            if(code === 13){
-                term.write("\r\n")
-                console.log("Command entered: ", output)
-                output = ""
-                term.write("$: ")
-                return;
+        async function fit_terminal(){
+                fit.fit()
+                await invoke("pty_resize", {rows: term.rows, cols: term.cols})
             }
-            if(code === 127){
-                if(output.length > 0){
-                    output = output.slice(0, -1)
-                    term.write("\b \b")
+
+            function write_to_terminal(data: string){
+                return new Promise<void>((response) => {
+                    term.write(data, () => response())
+                })
+            }
+
+            function write_to_pty(data: string){
+                invoke("pty_write", {data})
+            }
+
+            function init_shell(){
+                invoke("create_shell_process").catch((error) => {
+                    console.error("Error creating shell:", error)
+                })
+            }
+
+            async function read_from_pty(){
+                const data = await invoke<string>("pty_read")
+
+                if(data){
+                    await write_to_terminal(data)
                 }
-                return
+                window.requestAnimationFrame(read_from_pty)
             }
-            output += data
-            term.write(data)
-        })
+        init_shell()
+        fit_terminal()
+        addEventListener("resize", fit_terminal)
+        term.onData(write_to_pty)
+        window.requestAnimationFrame(read_from_pty)
     })
 </script>
 
